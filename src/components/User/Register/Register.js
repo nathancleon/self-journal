a03181a243f4b89e2ec8db2e62483e84fa2e29b4
Nathaniel Collins <nathancleon@gmail.com>
2019-10-08 15:35:45 -0400

    remove unnecessary escape characters per eslint recommendations

--- src/components/User/Register/Register.js
+++ src/components/User/Register/Register.js
@@ -39,7 +39,7 @@ class Register extends Component {
   }
 
   emailAndPasswordValidation() {
-    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (regex.test(String(this.state.email).toLowerCase()) === false) {
       this.setState({
         emailError: true
