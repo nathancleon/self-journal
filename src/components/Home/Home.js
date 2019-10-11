660b0cb6d8d35a287d2e663bbf09df711b7675cd
Nathaniel Collins <nathancleon@gmail.com>
2019-10-11 14:21:17 -0400

    somehow this wasn't in the home component??? probably didn't accept change in merge

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import moment from "moment";
 import { fetchAllJournalData } from "../../actions/JournalActions";
 import { Loading } from "../Loading/Loading";
 import { promptData } from "../JournalListContainer/JournalSelected/PromptResponses/promptData";
+import Chart from "./Chart";
 import {
   HomeContainer,
   ContentContainer,
